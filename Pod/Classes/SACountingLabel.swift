27d030b3eb557cfc99a37cfc4083639b5d744986
Gerardo Ramirez <gerardo.ramirez@isol.com.mx>
2016-11-14 19:08:16 -0600

    Swift 3 and Currency

--- Pod/Classes/SACountingLabel.swift
+++ Pod/Classes/SACountingLabel.swift
@@ -9,32 +9,33 @@
 import Foundation
 import UIKit
 
-public class SACountingLabel: UILabel {
+open class SACountingLabel: UILabel {
     
     let kCounterRate: Float = 3.0
     
     public enum AnimationType {
-        case Linear
-        case EaseIn
-        case EaseOut
-        case EaseInOut
+        case linear
+        case easeIn
+        case easeOut
+        case easeInOut
     }
     
     public enum CountingType {
-        case Int
-        case Float
-        case Custom
+        case int
+        case float
+        case custom
+        case currency
     }
     
     var start: Float = 0.0
     var end: Float = 0.0
-    var timer: NSTimer?
-    var progress: NSTimeInterval!
-    var lastUpdate: NSTimeInterval!
-    var duration: NSTimeInterval!
+    var timer: Timer?
+    var progress: TimeInterval!
+    var lastUpdate: TimeInterval!
+    var duration: TimeInterval!
     var countingType: CountingType!
     var animationType: AnimationType!
-    public var format: String?
+    open var format: String?
     
     var currentValue: Float {
         if (progress >= duration) {
@@ -45,7 +46,7 @@ public class SACountingLabel: UILabel {
         return start + (update * (end - start));
     }
     
-    public func countFrom(fromValue: Float, to toValue: Float, withDuration duration: NSTimeInterval, andAnimationType aType: AnimationType, andCountingType cType: CountingType) {
+    open func countFrom(_ fromValue: Float, to toValue: Float, withDuration duration: TimeInterval, andAnimationType aType: AnimationType, andCountingType cType: CountingType) {
         
         // Set values
         self.start = fromValue
@@ -54,7 +55,7 @@ public class SACountingLabel: UILabel {
         self.countingType = cType
         self.animationType = aType
         self.progress = 0.0
-        self.lastUpdate = NSDate.timeIntervalSinceReferenceDate()
+        self.lastUpdate = Date.timeIntervalSinceReferenceDate
         
         // Invalidate and nullify timer
         killTimer()
@@ -66,16 +67,22 @@ public class SACountingLabel: UILabel {
         }
         
         // Create timer
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "updateValue", userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(SACountingLabel.updateValue), userInfo: nil, repeats: true)
     }
     
-    func updateText(value: Float) {
+    func updateText(_ value: Float) {
+        
         switch countingType! {
-        case .Int:
+        case .currency:
+            let number = NumberFormatter()
+            number.numberStyle = .currency
+            number.maximumFractionDigits = 0
+            self.text = number.string(from: NSNumber(value:value))// "\(Int(value))"
+        case .int:
             self.text = "\(Int(value))"
-        case .Float:
+        case .float:
             self.text = String(format: "%.2f", value)
-        case .Custom:
+        case .custom:
             if let format = format {
                 self.text = String(format: format, value)
             } else {
@@ -87,7 +94,7 @@ public class SACountingLabel: UILabel {
     func updateValue() {
         
         // Update the progress
-        let now = NSDate.timeIntervalSinceReferenceDate()
+        let now = Date.timeIntervalSinceReferenceDate
         progress = progress + (now - lastUpdate)
         lastUpdate = now
         
@@ -106,15 +113,15 @@ public class SACountingLabel: UILabel {
         timer = nil
     }
     
-    func updateCounter(t: Float) -> Float {
+    func updateCounter(_ t: Float) -> Float {
         switch animationType! {
-        case .Linear:
+        case .linear:
             return t
-        case .EaseIn:
+        case .easeIn:
             return powf(t, kCounterRate)
-        case .EaseOut:
+        case .easeOut:
             return 1.0 - powf((1.0 - t), kCounterRate)
-        case .EaseInOut:
+        case .easeInOut:
             var t = t
             var sign = 1.0;
             let r = Int(kCounterRate)
